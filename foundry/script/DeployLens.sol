43b148899043a8b8edce5a64f6dde65305f9af31
nezz0746 <nezz0746@gmail.com>
2023-06-10 19:57:04 +0200

    add script instructions pseudo-code

---
+++ foundry/script/DeployLens.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+/// TODO: Install Lens Protocol
+
+contract DeployModules is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        // Deploy Lens Module
+
+        // White List Lens Module
+
+        vm.stopBroadcast();
+    }
+}
---
+++ foundry/script/DeployMap.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+/// TODO: Install Lens-Map
+/// TODO: Import Map Module
+
+contract DeployMap is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        // Deploy Map
+
+        // Deploy Map Verification Module
+
+        vm.stopBroadcast();
+    }
+}
